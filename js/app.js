4e553bacb70d096770c917f07f5edeff2cf7fdf1
Nikita Lvov <niklvov@gmail.com>
2013-08-08 19:09:42 +0400

    model and view impovements

--- js/app.js
+++ js/app.js
@@ -1,175 +1,107 @@
 var CANVAS_WIDTH = 300;
 var CANVAS_HEIGHT = 300;
+var ANIM_TIME = 100;
 var canvas;
 
 var PatternPart = Backbone.Model.extend({
     initialize: function () {
         this.set({
+            id: Math.round(Math.random() * 1000000),
             angle: 0,
-            color: null,
+            color: "rgba(0, 0, 0, 0)",
             opacity: 1,
-            width: 100,
-            height: 100,
+            width: 1,
+            height: 1,
             overlay: null,
-            count: 0,
-            placement_type: "circle", //random, circle
-            angle_delta: 0,
-            grid_type: 9, //9, 25, 49
+            count: 5,
+            placement: "random", //random, circle
+            angle_delta: 50,
+            grid: 9, //9, 25, 49
             x: 0,
-            y: 0
+            y: 0,
+            radius: 100
         });
     }
 });
 
 var PatternPartControlsView = Backbone.View.extend({
     tagName: "div",
-    className: "row",
-    template: _.template('<div class="row">
-        <div class="pattern-part">
-    <div class="col-lg-4 col-sm-4">
-        <img class="part-img"
-        src="http://icons.iconarchive.com/icons/enhancedlabs/lha-objects/128/Filetype-IMG-icon.png">
-        </div>
-        <div class="col-lg-8 col-sm-8 form-horizontal pattern-part-controls">
-            <div class="form-group">
-                <label class="col-lg-3 control-label" for="angle-of-obj">Angle</label>
-
-                <div class="col-lg-9">
-                    <input id="angle-of-obj" type="number" class="slider form-control" value=""
-                    data-slider-min="0"
-                    data-slider-max="360" data-slider-step="1" data-slider-value="0">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label" for="opacity-of-obj">Opacity</label>
-
-                    <div class="col-lg-9">
-                        <input id="opacity-of-obj" type="number" class="slider form-control" value=""
-                        data-slider-min="0"
-                        data-slider-max="1" data-slider-step="0.01" data-slider-value="1">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-lg-3 control-label" for="width-of-obj">Width</label>
-
-                        <div class="col-lg-9">
-                            <input id="width-of-obj" type="number" class="slider form-control" value=""
-                            data-slider-min="0"
-                            data-slider-max="5" data-slider-step="0.01" data-slider-value="1">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-lg-3 control-label" for="height-of-obj">Height</label>
-
-                            <div class="col-lg-9">
-                                <input id="height-of-obj" type="number" class="slider form-control" value=""
-                                data-slider-min="0"
-                                data-slider-max="5" data-slider-step="0.01" data-slider-value="1">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-lg-3 control-label">Grid</label>
-
-                                <div class="col-lg-9">
-                                    <div class="radio-inline">
-                                        <label>
-                                            <input type="radio" name="gridsize" id="gridsize1" value="9" checked>
-                                            9
-                                            </label>
-                                        </div>
-                                        <div class="radio-inline">
-                                            <label>
-                                                <input type="radio" name="gridsize" id="gridsize2" value="25">
-                                                25
-                                                </label>
-                                            </div>
-                                            <div class="radio-inline">
-                                                <label>
-                                                    <input type="radio" name="gridsize" id="gridsize3" value="49">
-                                                    49
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-lg-3 control-label" for="count-of-obj">Parts</label>
-
-                                            <div class="col-lg-9">
-                                                <input id="count-of-obj" type="number" class="slider form-control" value=""
-                                                data-slider-min="0"
-                                                data-slider-max="40" data-slider-step="1" data-slider-value="5">
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="col-lg-3 control-label">Placement</label>
-
-                                                <div class="col-lg-9">
-                                                    <div class="radio-inline">
-                                                        <label>
-                                                            <input type="radio" name="gridsize-of-obj" id="placement1" value="random" checked>
-                                                            Random
-                                                            </label>
-                                                        </div>
-                                                        <div class="radio-inline">
-                                                            <label>
-                                                                <input type="radio" name="gridsize-of-obj" id="placement2" value="circle">
-                                                                Circle
-                                                                </label>
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <label class="col-lg-3 control-label" for="color-of-obj">Color</label>
-
-                                                                <div class="col-lg-9">
-                                                                    <div class="input-append color" data-color-format="rgba">
-                                                                        <input id="color-of-obj" class="colorpicker input-sm" type="text" value="rgba(0, 0, 0, 0)" >
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>'),
-    init_part_controls: function () {
+    className: "row pattern-part thumbnail",
+    template: _.template($("#part-settings-tmpl").html()),
+    init_controls: function () {
+        //console.log(this.model.attributes);
+        this.$el.find('.colorpicker').colorpicker({format: "rgba"});
         this.$el.find('.slider').slider().on('slide', function (e) {
-            e.value = e.value.toPrecision(2);
+            e.value = e.value.toFixed(2);
             $(this).slider('setValue', e.value);
         });
+        this.$el.find('input[type="radio"]').change(_.bind(this.change_settings_order, this));
+        this.$el.find('.count .slider').on("slide", _.bind(this.change_settings_order, this));
+        this.change_settings_order();
+        return this;
+    },
+    change_settings_order: function () {
+        var isCircle = this.$el.find('.placement input:checked').val() == "circle";
+        var isRandom = this.$el.find('.placement input:checked').val() == "random";
+        var isOneItem = this.$el.find('.count .slider').slider('getValue') == 1;
+        if (isCircle || isOneItem) {
+            this.$el.find('.form-group.x').show(ANIM_TIME);
+            this.$el.find('.form-group.y').show(ANIM_TIME);
+        } else {
+            this.$el.find('.form-group.x').hide(ANIM_TIME);
+            this.$el.find('.form-group.y').hide(ANIM_TIME);
+        }
+
+        if (isCircle) {
+            this.$el.find('.form-group.angle-delta').show(ANIM_TIME);
+            this.$el.find('.form-group.radius').show(ANIM_TIME);
+        } else if (isRandom) {
+            this.$el.find('.form-group.angle-delta').hide(ANIM_TIME);
+            this.$el.find('.form-group.radius').hide(ANIM_TIME);
+        }
+    },
+    place: function () {
+        var last = $('.control-panel .row.pattern-part:last-child');
+        if (last.length > 0)
+            last.after(this.$el);
+        else
+            $('.control-panel .row:first-child').after(this.$el);
+        return this;
     },
     render: function () {
         this.$el.html(this.template(this.model.attributes));
-        this.init_part_controls();
-        console.log(this);
         return this;
+    },
+    events: {
+        "click button.close": "remove",
+        "slide input": "slide",
+        "change input[type=radio]": "radio_changed"
+    },
+    remove: function () {
+        this.$el.remove();
+    },
+    slide: function (ev) {
+        var p_name = $(ev.target).attr('id').replace("-of-obj", "").replace("-", "_");
+        this.model.set(p_name, parseFloat(ev.value));
+        //console.log(this.model.attributes);
+    },
+    radio_changed: function (ev) {
+        var p_name = $(ev.target).attr('name').replace("-of-obj", "").replace("-", "_");
+        if (p_name == "grid")
+            this.model.set(p_name, parseInt($(ev.target).val()));
+        else if (p_name == "placement")
+            this.model.set(p_name, $(ev.target).val());
+        //console.log(this.model.attributes);
     }
 });
-
-var model = new PatternPart({
-    type: "img"
-});
-
-var view = new PatternPartControlsView({
-    model: model
-});
-
-view.render();
 
 
 function init() {
     console.log("init app and controls");
-    $('.slider').slider({handle:"triangle"}).on('slide', function (e) {
-        e.value = e.value.toPrecision(2);
-        $(this).slider('setValue', e.value);
-    });
-    $('.colorpicker').colorpicker({format:"rgba"});
     $('#upload.btn').click(upload);
     $('#render.btn').click(render);
     $('#file-uploader').change(handle_image);
     init_canvas();
-}
-
-function add_part() {
-
 }
 
 function init_canvas() {
@@ -188,18 +120,23 @@ function handle_image(e) {
     reader.onload = function (event) {
         var imgObj = new Image();
         imgObj.src = event.target.result;
+        var model = new PatternPart({type: "img", img: imgObj});
+        var view = new PatternPartControlsView({model: model});
+        view.render().place().init_controls();
 
         imgObj.onload = function () {
-            var image = new fabric.Image(imgObj);
-            image.set({
-                left: 250,
-                top: 250,
-                angle: 20,
-                padding: 10,
-                cornersize: 10
-            });
-            canvas.add(image);
-        }
+            /*
+             var image = new fabric.Image(imgObj);
+             image.set({
+             left: 250,
+             top: 250,
+             angle: 20,
+             padding: 10,
+             cornersize: 10
+             });
+             canvas.add(image);*/
+        };
+        $(e.target).val("");
     };
     reader.readAsDataURL(e.target.files[0]);
 }
@@ -216,13 +153,3 @@ function render() {
 }
 
 init();
-
-
-//test
-// Check for the various File API support.
-/*
- if (window.File && window.FileReader && window.FileList && window.Blob) {
- // Great success! All the File APIs are supported.
- } else {
- alert('The File APIs are not fully supported in this browser.');
- }*/
