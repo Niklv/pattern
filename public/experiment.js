989a3dcec706bdf4d9eaf90c51b9d69c629e6107
Nikita Lvov <niklvov@gmail.com>
2013-11-23 00:42:21 +0400

    make ma code clear

--- public/experiment.js
+++ public/experiment.js
@@ -1,20 +1,11 @@
-var M = Backbone.Model.extend({
-    initialize: function () {
-        this.bind("remove", function () {
-            ///console.log("remove")
-        })
-    }
-});
-var C = Backbone.Collection.extend({
-    model: M,
-    add: function(){
-        console.log("add");
-    }
-});
+var B = function(){
+    console.log("constructor B");
+};
 
-var c = new C;
-for (var i = 0; i < 10; i++)
-    c.add({});
-console.log(c.length);
-c.remove(c.models);
-console.log(c.length);
+var A = function(){
+    console.log("constructor A");
+};
+
+A.prototype.b = new B();
+
+var a = new A();
