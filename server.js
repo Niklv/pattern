716e06a35279f49e4cb841e9a785b6e3f66c3791
Nikita Lvov <niklvov@gmail.com>
2013-11-28 14:20:55 +0400

    add sample sprite convertation add new sample pack

--- server.js
+++ server.js
@@ -13,31 +13,51 @@ app.all('/imgtob64', function (req, res) {
     if (url != null) {
         try {
             utils.imgtob64(url, function (image, prefix) {
-                res.json({image: image, prefix: prefix, err:null});
+                res.json({image: image, prefix: prefix, err: null});
             });
-        } catch (err){
-            res.json({err:err.message});
+        } catch (err) {
+            res.json({err: err.message});
         }
     } else {
-        res.json({err:"check parameters"});
+        res.json({err: "check parameters"});
     }
 });
 
-/*app.get('/library', function (req, res) {
-    var id = req.param("id");
-    if (id != null) {
+//for convertation to b64 large count of images
+/*app.all('/createB64', function (req, res) {
+ var fs = require('fs'), async = require('async'), arr = new Array(100);
+ for (var i = 0; i < arr.length; i++)
+ arr[i] = i + 1;
+ async.forEach(arr, function (val, cb) {
+ utils.imgtob64("http://localhost:20201/img/calculated/sample_" + val + ".png", function (image, prefix) {
+ var str_to_save = JSON.stringify({image: image, prefix: prefix, err: null});
+ fs.writeFile("public/img/calculated/sample_1_file_" + val + ".png.json", str_to_save, function (err) {
+ if (err)
+ console.log(err);
+ cb();
+ });
+ });
+ }, function () {
+ res.send(200);
+ });
+ });*/
 
-        try {
-            utils.imgtob64(url, function (image, prefix) {
-                res.json({image: image, prefix: prefix, err:null});
-            });
-        } catch (err){
-            res.json({err:err.message});
-        }
-    } else {
-        res.send(500);
-    }
-});*/
+
+
+/*app.get('/library', function (req, res) {
+ var id = req.param("id");
+ if (id != null) {
+ try {
+ utils.imgtob64(url, function (image, prefix) {
+ res.json({image: image, prefix: prefix, err:null});
+ });
+ } catch (err){
+ res.json({err:err.message});
+ }
+ } else {
+ res.send(500);
+ }
+ });*/
 
 app.configure("production", function () {
     app.listen(20200);
