2f3d05e77d94be877e311eb6ec6377d51b2763ba
Nikita Lvov <niklvov@gmail.com>
2014-02-07 15:08:06 +0400

    new colorpicker

--- public/js/app/colorpicker/view.js
+++ public/js/app/colorpicker/view.js
@@ -1,10 +1,14 @@
 var ColorpickerView = Backbone.View.extend({
-    $rgb: null,
+    $color: null,
     $alpha: null,
-    $result_color: null,
+    $result: null,
     template_color: "<input class='hex-color' type='text'><div class='picker'><div class='map'><div class='pointer'></div></div><div class='column value'><div class='selector'></div></div></div></div>",
     template_alpha: "<input class='alpha' type='text'><div class='op-picker'><div class='selector'></div></div>",
+    template_result: "<input type='text' class='color-result'>",
     initialize: function () {
-        this.$el.css({height: "100px", width: "100px", "background-color": "red"});
+        this.$color = $(this.template_color);
+        this.$alpha = $(this.template_alpha);
+        this.$result = $(this.template_result);
+        this.$el.append(this.$color, this.$alpha, this.$result);
     }
 });
